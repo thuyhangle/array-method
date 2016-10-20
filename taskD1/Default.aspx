d9a0774d00bacf56a01ce7a93691bd29b65367ba
Le Thuy Hang <t4leth00@students.oamk.fi>
2016-10-20 06:19:32 +0300

    task D1

---
+++ taskD1/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="taskD1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Button OnClick="btnClicked" runat="server" Text="Show Array" />
+    <asp:TextBox ID="cityName" runat="server" Width="200" Height="50"></asp:TextBox>
+        <br />
+    <asp:TextBox ID="numbersList" runat="server" width="1000" Height="50"></asp:TextBox>
+    </div>
+    </form>
+</body>
+</html>
---
+++ taskD1/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace taskD1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClicked(object sender, EventArgs e)
+        {
+            string[] citys = new string[4];
+            citys[0] = "Helsinki";
+            citys[1] = "Oulu";
+            citys[2] = "Hanoi";
+            citys[3] = "HCMC";
+            cityName.Text = string.Join(" , ", citys);
+
+            int[] numbers = new int[500];
+            Random rdn = new Random();
+            for (int i = 0; i < 500; i++ )
+            {
+                numbers[i] = rdn.Next(0, 501);
+            }
+            numbersList.Text = string.Join(" , ", numbers);
+        }
+    }
+}
